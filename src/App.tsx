b8abd9665630765a3cf1ebd7dd92c7615c5aedba
Vivek <vivekmunde@gmail.com>
2022-07-15 13:06:00 +0530

    tailwind setup, react router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import './App.css';
 
 import { Provider as ReduxStoreProvider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import store from './redux/store';
+import Home from './screens/home';
 
 function App() {
 
   return (
     <ReduxStoreProvider store={store}>
-      {/* APP */}
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
     </ReduxStoreProvider>
   );
 }
