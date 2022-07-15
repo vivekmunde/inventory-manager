7151d1294cc3cc0aa62e7aeceb81f70fc9616d79
Vivek <vivekmunde@gmail.com>
2022-07-15 13:20:54 +0530

    app overlay spinner for loading state

---
+++ src/components/app-overlay-spinner/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import AppOverlaySpinnerView from './app-overlay-spinner-view';
+
+const AppOverlaySpinner: React.FC = () => <AppOverlaySpinnerView visible />;
+
+export default AppOverlaySpinner;
