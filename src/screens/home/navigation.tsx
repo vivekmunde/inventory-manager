7ade09bfafe1ffba589710b00389fbc424301848
Vivek <vivekmunde@gmail.com>
2022-07-16 12:50:47 +0530

    sort categories by name

--- src/screens/home/navigation.tsx
+++ src/screens/home/navigation.tsx
@@ -1,9 +1,14 @@
+import orderBy from 'lodash/orderBy';
 import React from 'react';
 import { shallowEqual, useSelector } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
 
 import { TState } from '../../redux/types';
 import { TCategory } from '../../types';
+
+const sortCategories = (list: TCategory[]) => (
+  orderBy(list, [(it) => it.title?.toLowerCase()], ['asc'])
+);
 
 const Navigation: React.FC = () => {
   const routeLocation = useLocation();
@@ -22,7 +27,7 @@ const Navigation: React.FC = () => {
         <Link to="/home" className={linkClassName('/home')}>
           All
         </Link>
-        {categories.map((category) => (
+        {sortCategories(categories).map((category) => (
           <Link key={category.id} to={`/inventory/${category.id}`} className={linkClassName(`/inventory/${category.id}`)}>
             {category.title}
           </Link>
